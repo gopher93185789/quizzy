3be4c347a55d763ef3842e9b92cfb9e5eb85a5aa
script kiddie <anish.chittu2007@gmail.com>
2025-02-19 19:28:15 +0100

    made textcard accept children

--- src/components/TextCard.tsx
+++ src/components/TextCard.tsx
@@ -1,20 +1,25 @@
+import React from "react";
+
 interface TextCardProps {
-    title: string
-    titleColor: string
-    text: string
+  title: string;
+  titleColor: string;
+  text: string;
+  children?: React.ReactElement;
 }
 
-export function TextCard({text, title, titleColor}:TextCardProps) {
-    return (
-      <>
-        <div className="h-[80vh]  shadow-inset p-10 w-full sm:w-1/4 flex flex-col rounded-xl shadow-lg bg-[#050505]">
-          <p className={`w-full text-4xl sm:text-5xl ${titleColor} font-bold`}>
-            {title}
-          </p>
-          <p className="w-full text-lg font-medium mt-5 overflow-hidden text-white text-opacity-50">
-            {text}
-          </p>
-        </div>
-      </>
-    );
+export function TextCard({ text, title, titleColor, children }: TextCardProps) {
+  return (
+    <>
+      <div className="h-[80vh] overflow-hidden  shadow-inset p-10 w-full sm:w-1/4 flex flex-col rounded-xl shadow-lg bg-[#050505]">
+        <p className={`w-full text-4xl sm:text-5xl ${titleColor} font-bold`}>
+          {title}
+        </p>
+        <p className="w-full text-lg font-medium mt-5 overflow-hidden text-white text-opacity-50">
+          {text}
+        </p>
+
+        {children}
+      </div>
+    </>
+  );
 }
