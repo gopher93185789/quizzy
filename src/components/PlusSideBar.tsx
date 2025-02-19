140739ef9cc1d295cc5eba33a363a04268444c17
script kiddie <anish.chittu2007@gmail.com>
2025-02-19 18:19:49 +0100

    fixed plus component

--- src/components/PlusSideBar.tsx
+++ src/components/PlusSideBar.tsx
@@ -1,12 +1,8 @@
-interface PlusSideBarProps {
-    top?:string
-    left?:string
-}
 
-export function PlusSideBar({top="0", left="0"}: PlusSideBarProps) {
+export function PlusSideBar() {
   return (
     <>
-      <div className={`w-32 z-50 sm:absolute hidden left-${left} top-${top}`}>
+      <div className={`w-32 z-50 hidden sm:block absolute left-0`}>
         <div className="w-full h-full flex justify-center items-center">
           <p className="h-fit mr-0.5 p-0 m-0 w-fit text-white text-3xl">+</p>
         </div>
