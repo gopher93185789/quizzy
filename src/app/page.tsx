458161cf0395362504a93d1167a10e130714e4fb
script kiddie <anish.chittu2007@gmail.com>
2025-02-20 16:29:59 +0100

    added a section for a product demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,10 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <div className="fixed hidden lg:flex z-10 left-0  h-full w-16 border border-white/10 border-l-0 border-y-0" />
-
+      <div className="fixed hidden lg:flex z-10 left-0 h-full w-16 border border-white/10 border-l-0 border-y-0" />
       <div className="w-full h-[400px] lg:h-[800px] overflow-hidden  relative bg-grid flex lg:items-center justify-center">
         <Spotlight />
 
-        <div className="h-1/4 bottom-0 w-full absolute bg-gradient-to-t from-primary" />
         <div className="h-fit mt-36 select-none  lg:mt-0 w-full lg:w-1/2  flex flex-col items-center justify-center">
           <h1 className="text-4xl overflow-hidden text-nowrap lg:text-7xl h-14 lg:h-20 font-bold text-center bg-clip-text text-transparent antialiased bg-gradient-to-b from-neutral-50 to-neutral-400/40">
             Quizzes, Simplified
@@ -31,7 +29,26 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="w-full flex flex-col items-center bg-primary">
+      <div className="w-full h-175 bg-grid relative p-0 m-0 flex overflow-hidden items-center justify-center">
+        <PlusSideBar />
+        <div className="h-1/4 bottom-0 z-5 w-full absolute bg-gradient-to-t from-primary" />
+        <div className="absolute w-full -bottom-15 h-full">
+          <div className="w-full h-full flex items-center justify-center">
+            <div className="bg-black shadow-outset  w-300 aspect-video overflow-hidden rounded-lg rotate-2">
+              <iframe
+                width="100%"
+                height="100%"
+                src="https://www.youtube.com/embed/T-qAtAKjqwc?si=oaAk0d2dmddIEs21"
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+              ></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full flex flex-col items-center py-20 bg-primary">
         <PlusSideBar />
         <div className="flex flex-col px-5 lg:flex-row justify-center gap-5 w-full h-full">
           <TextCard
--- src/components/AsciiIcons.tsx
+++ src/components/AsciiIcons.tsx
@@ -2,25 +2,25 @@ export function Shield() {
     return <>
         <div className="h-full w-full flex justify-center select-none items-center">
             <div className="h-fit w-fit group">
-                <pre className='text-white/50 w-full text-center sm:text-md'>              anish              </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md'>          X$$$     $$$X          </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md'>     $$$$$$    $$$    $$$$$X     </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md'>$$$X       $$$$ $ $$$$       $$$$</pre>
-                <pre className='text-white/50 w-full text-center sm:text-md'>$X  $$$$$$$     $     $$$$$$$  $$</pre>
-                <pre className='text-white/50 w-full text-center sm:text-md'>$$ X$           $           $$ $$</pre>
-                <pre className='text-white/50 w-full text-center sm:text-md'>$$ ss           $           $$ $$</pre>
-                <pre className='text-white/50 w-full text-center sm:text-md'>$$  $$          $          $$  $$</pre>
-                <pre className='text-white/50 w-full text-center sm:text-md'>X$  $$          $          $$  $$</pre>
-                <pre className='text-white/50 w-full text-center sm:text-md'>$$$ $$          $          $$ $$X</pre>
-                <pre className='text-white/50 w-full text-center sm:text-md'> $$  $$         $         $$  $$ </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md'>  $$ $$$        $        $$$ $$  </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md'>  $$$ X$$       $       $$X $$X  </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md'>   $$$ X$$      $      $$X X$$   </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md'>    X$$  $$     $     $$X $$$    </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md'>       $$$  $$$ $ $$$  $$$       </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md'>         $$$  X$$$$  $$$         </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md'>           $$$     $$$           </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md'>             $342e3$             </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md'>              anish              </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md'>          X$$$     $$$X          </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md'>     $$$$$$    $$$    $$$$$X     </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md'>$$$X       $$$$ $ $$$$       $$$$</pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md'>$X  $$$$$$$     $     $$$$$$$  $$</pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md'>$$ X$           $           $$ $$</pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md'>$$ ss           $           $$ $$</pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md'>$$  $$          $          $$  $$</pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md'>X$  $$          $          $$  $$</pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md'>$$$ $$          $          $$ $$X</pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md'> $$  $$         $         $$  $$ </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md'>  $$ $$$        $        $$$ $$  </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md'>  $$$ X$$       $       $$X $$X  </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md'>   $$$ X$$      $      $$X X$$   </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md'>    X$$  $$     $     $$X $$$    </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md'>       $$$  $$$ $ $$$  $$$       </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md'>         $$$  X$$$$  $$$         </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md'>           $$$     $$$           </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md'>             $342e3$             </pre>
             </div>
         </div>
     </>
@@ -31,27 +31,27 @@ export function Head() {
     return <>
         <div className="h-full w-full flex justify-center select-none items-center">
             <div className="h-fit w-fit group">
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>                 @@@@@@@s@@            </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>            @@s@@@@@    @s@@@@@@        </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>          @@s@@               @@s@      </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>        @s@@                    @s@@    </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>       @@s@        @@   @@        @s@   </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>      @s@        @@@@@@@@@@@@      @s@  </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>     @s@       @@@         @@@      @s@ </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>     @s@     @@@@ @@@@@@@@@ @@@@    @@s@</pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>    @@s@     @@@@ @@     @@ @@@@     @s@</pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>    @@s@       @@ @@     @@ @@       @s@</pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>    @s@@     @@@@ @@     @@ @@@@@   @@s@</pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>    @s@@       @@  @@@@@@@@ @@      @s@ </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>   @s@@        @@@@       @@@@     @@s@ </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>  @@@s           @@@@@@@@@@@      @s@@  </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'> @@s@                 @@         @@s@   </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>@@@@dd@@               @@        @s@     </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>  @@s@@@              @@        s@      </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>    @s@@              @@       @s@      </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>     @s@             @@@       @s@      </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>      @@@s@        @@@@        @s@      </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>        @s@@@@@@@@@@@           @s      </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>                 @@@@@@@s@@            </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>            @@s@@@@@    @s@@@@@@        </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>          @@s@@               @@s@      </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>        @s@@                    @s@@    </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>       @@s@        @@   @@        @s@   </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>      @s@        @@@@@@@@@@@@      @s@  </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>     @s@       @@@         @@@      @s@ </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>     @s@     @@@@ @@@@@@@@@ @@@@    @@s@</pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>    @@s@     @@@@ @@     @@ @@@@     @s@</pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>    @@s@       @@ @@     @@ @@       @s@</pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>    @s@@     @@@@ @@     @@ @@@@@   @@s@</pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>    @s@@       @@  @@@@@@@@ @@      @s@ </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>   @s@@        @@@@       @@@@     @@s@ </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>  @@@s           @@@@@@@@@@@      @s@@  </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'> @@s@                 @@         @@s@   </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>@@@@dd@@               @@        @s@     </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>  @@s@@@              @@        s@      </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>    @s@@              @@       @s@      </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>     @s@             @@@       @s@      </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>      @@@s@        @@@@        @s@      </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>        @s@@@@@@@@@@@           @s      </pre>
             </div>
         </div>
     </>
@@ -62,25 +62,25 @@ export function World() {
     return <>
         <div className="h-full w-full flex justify-center select-none items-center">
             <div className="h-fit w-fit group">
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>  @@@@@@@@@@@@@@@@@@@@     @@@@@@@@       </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>@@@@@@@@@@@@@@@@@@@@@@   @@@@@@@@@@@@     </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>@@@@                     @@@@@@@@@@@@@    </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>@@@@                    @@@@@@@@@@@@@@    </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>@@@@                     @@@@@@@@@@@@@    </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>@@@                       @@@@@@@@@@      </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>                       @@@@@  @@   @@@@@  </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>        @@@@@        @@@@@@@@@@@@@@@@@@@@ </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>      @@@@@@@@@@     @@@@@@@@@@@@@@@@@@@@@</pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>    @@@@@@@@@@@@@   @@@@@@@@@@@@@@@@@@@@@@</pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>    @@@@@@@@@@@@@     @@@@@@@@@@@@@@@@@@  </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>     @@@@@@@@@@@@         @@@@@@@@@@      </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>   @@  @@@@@@@@ @@                        </pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>  @@@@@@@@  @@@@@@@@                   @@@</pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>@@@@@@@@@@@@@@@@@@@@@                  @@@</pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>@@@@@@@@@@@@@@@@@@@@@@                 @@@</pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>@@@@@@@@@@@@@@@@@@@@@@                @@@@</pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>@@@@@@@@@@@@@@@@@@@@@   @@@@@@@@@@@@@@@@@@</pre>
-                <pre className='text-white/50 w-full text-center sm:text-md text-sm'>   @@@@@@@@@@@@@@@      @@@@@@@@@@@@@@@@  </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>  @@@@@@@@@@@@@@@@@@@@     @@@@@@@@       </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>@@@@@@@@@@@@@@@@@@@@@@   @@@@@@@@@@@@     </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>@@@@                     @@@@@@@@@@@@@    </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>@@@@                    @@@@@@@@@@@@@@    </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>@@@@                     @@@@@@@@@@@@@    </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>@@@                       @@@@@@@@@@      </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>                       @@@@@  @@   @@@@@  </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>        @@@@@        @@@@@@@@@@@@@@@@@@@@ </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>      @@@@@@@@@@     @@@@@@@@@@@@@@@@@@@@@</pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>    @@@@@@@@@@@@@   @@@@@@@@@@@@@@@@@@@@@@</pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>    @@@@@@@@@@@@@     @@@@@@@@@@@@@@@@@@  </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>     @@@@@@@@@@@@         @@@@@@@@@@      </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>   @@  @@@@@@@@ @@                        </pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>  @@@@@@@@  @@@@@@@@                   @@@</pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>@@@@@@@@@@@@@@@@@@@@@                  @@@</pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>@@@@@@@@@@@@@@@@@@@@@@                 @@@</pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>@@@@@@@@@@@@@@@@@@@@@@                @@@@</pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>@@@@@@@@@@@@@@@@@@@@@   @@@@@@@@@@@@@@@@@@</pre>
+                <pre className='text-white/50 antialiased w-full text-center sm:text-md text-sm'>   @@@@@@@@@@@@@@@      @@@@@@@@@@@@@@@@  </pre>
             </div>
         </div>
     </>
