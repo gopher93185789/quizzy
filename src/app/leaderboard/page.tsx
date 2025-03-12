4ef5ec34fa10acf14b07a968b00fc0a84d842af1
yn1243 <102717@glr.nl>
2025-03-12 09:01:59 +0000

    add basic leaderboard

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,8 +1,33 @@
+
+interface PlayerScoreProps {
+  PlayerName: string,
+  PlayerScore: string
+}
+
+const PlayerScore = ({PlayerName, PlayerScore}:PlayerScoreProps) => {
+  return <>
+    <div className="w-full flex flex-row gap-20">
+      <p className="text-white w-full text-5xl font-jersey text-center">{PlayerName}</p>
+      <p className="text-white w-full text-5xl font-jersey text-center">{PlayerScore}</p>
+    </div>
+  </>
+}
+
 export default function Leaderboard() {
   return (
     <>
-      <div className="w-full h-[calc(100vh-3rem)] bg-primary flex flex-col items-center">
-        
+      <div className="w-full h-[calc(100vh-3rem)] bg-primary flex flex-col items-center justify-center">
+        <div className="bw-1/2  h-fit">
+          <p className="text-white text-6xl text-center">Top scores</p>
+          <div className="w-full mt-10 h-fit flex flex-col gap-10">
+              <PlayerScore PlayerName="SCOTT" PlayerScore="1372" />
+              <PlayerScore PlayerName="SCOTT" PlayerScore="1372" />
+              <PlayerScore PlayerName="SCOTT" PlayerScore="1372" />
+              <PlayerScore PlayerName="SCOTT" PlayerScore="1372" />
+              <PlayerScore PlayerName="SCOTT" PlayerScore="1372" />
+
+          </div>
+        </div>
       </div>
     </>
   );
